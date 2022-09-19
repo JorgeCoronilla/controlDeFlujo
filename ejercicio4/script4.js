edf812b337a91fd0573f5920774c5e3078e31517
JorgeCoronilla <jorge.coronilla.naranjo@gmail.com>
2022-09-19 19:23:58 +0200

    corrección ejercicio 4

--- ejercicio4/script4.js
+++ ejercicio4/script4.js
@@ -1,6 +1,6 @@
 var userOK = "Estudiante";
 var passwordOK = "batman17";
-var user, password;
+var user, password, contador = 3;
 var validation = false;
 alert("Hola");
 
@@ -9,6 +9,18 @@ do {
     password = prompt("Password");
     if (user == "Estudiante" && password == "batman17") {
         validation = true;
-    } else { alert("Usuario o password incorrecto. Por favor, inténtelo de nuevo");} }
-    while (validation == false);
-    alert("Usuario y password correctos.");
+    } else { contador --;
+        alert("Usuario o password incorrecto.");
+       }
+       if (contador > 1 && validation == false) {
+        alert("Piensa mejor, todavía tienes " + contador + " intentos.");
+       } else if (contador == 1 && validation == false) {
+        alert("Piensa mejor, todavía tienes " + contador + " intento.");
+       }
+     }
+    while (validation == false && contador > 0);
+    if (validation == true) {
+        alert("Usuario y password correctos.");} else {
+            alert("Has agotado tus tres intentos");
+        }
+    
